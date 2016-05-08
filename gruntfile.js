f67fc281e67e76577532ddc9b463c06d80804d69
Maxime Florile <florile.maxime@gail.com>
2016-05-08 22:53:23 +0200

    start desktop version

--- gruntfile.js
+++ gruntfile.js
@@ -1,59 +1,58 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'assets/css/build/index.min.css' : 'assets/sass/src/index.sass'
-                }
-            }
+    pkg: grunt.file.readJSON('package.json'),
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            'assets/css/build/*.css',
+            '*.html'
+          ]
         },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'assets/img/src/',
-                    src: ['**/*.{png,jpg,gif,svg}'],
-                    dest: 'assets/img/build/'
-                }]
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 9000,
-                    hostname: '*',
-                    keepalive: true
-                }
-            }
-        },
-        watch: {
-            sass: {
-                options: {
-                    livereload: true,
-                    open: true
-                },
-                files: ['assets/sass/src/*.sass'],
-                tasks: ['sass']
-            },
-            html: {
-                options: {
-                    livereload: true
-                },
-                files: ['index.html']
-            }
+        options: {
+          watchTask: true,
+          server: '.'
         }
-    });
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'assets/css/build/index.min.css': 'assets/sass/src/index.sass'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'assets/img/src/',
+          src: ['**/*.{png,jpg,gif,svg}'],
+          dest: 'assets/img/build/'
+        }]
+      }
+    },
+    watch: {
+      sass:{
+        files: ['assets/sass/src/*.sass'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-    grunt.registerTask('default', ['connect:server']);
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
 };
