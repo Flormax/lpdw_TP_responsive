c71978c5e83bb21ba15487c4d48f19eb911b4a67
Maxime Florile <florile.maxime@gail.com>
2016-05-12 23:18:04 +0200

    still working on desktop

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ burger.addEventListener('click', burgerIcon, false);
 var search = document.getElementById('search-header');
 var searchForm = document.getElementById('search-form');
 
-console.log(search);
-
 function searchIcon(){
-    console.log("toto");
     searchForm.className = searchForm.className == "visible" ? "" : "visible";
 };
 search.addEventListener('click', searchIcon, false);
