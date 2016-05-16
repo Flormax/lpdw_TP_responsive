c4d2c67a2f72899012da9a953de3aad7d17a863a
Maxime Florile <florile.maxime@gail.com>
2016-05-16 11:15:14 +0200

    still desktop version

--- index.js
+++ index.js
@@ -14,3 +14,49 @@ function searchIcon(){
     searchForm.className = searchForm.className == "visible" ? "" : "visible";
 };
 search.addEventListener('click', searchIcon, false);
+
+function searchIcon(){
+    searchForm.className = searchForm.className == "visible" ? "" : "visible";
+};
+search.addEventListener('click', searchIcon, false);
+
+
+var dropBtnBeaute = document.getElementById("drop-btn-beaute");
+var dropBtnCoiffure = document.getElementById("drop-btn-coiffure");
+var dropBtnMassageSpa = document.getElementById("drop-btn-massagespa");
+var dropBtnFormeMinceur = document.getElementById("drop-btn-formeminceur");
+var dropdowns = document.getElementsByClassName("dropdown-content");
+
+function dropdownClick(sectionId){
+  document.getElementById(sectionId).classList.toggle("show");
+  dropdownClose(sectionId);
+}
+
+function dropdownClose(id = null){
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var dropdown = dropdowns[i];
+    if (dropdown.classList.contains('show') && dropdown.id != id) {
+      dropdown.classList.remove('show');
+    }
+  }
+}
+
+dropBtnBeaute.addEventListener("click", function(){
+    dropdownClick('drop-content-beaute');
+}, false);
+dropBtnCoiffure.addEventListener("click", function(){
+    dropdownClick('drop-content-coiffure');
+}, false);
+dropBtnMassageSpa.addEventListener("click", function(){
+    dropdownClick('drop-content-massagespa');
+}, false);
+dropBtnFormeMinceur.addEventListener("click", function(){
+    dropdownClick('drop-content-formeminceur');
+}, false);
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    dropdownClose();
+  }
+}
